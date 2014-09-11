d5ce2e5f427706c5044eb1faa35863d04466d109
Samer Buna <samer.buna@gmail.com>
2014-09-11 10:57:05 -0700

    Cleanup

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,4 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/t', function(req, res) {
-  res.render('index', { title: 'CodingHouse', name: 'Nick' });
-});
-
-router.get('/t42', function(req, res) {
-  res.render('index42', { title: 'The Dark Side', name: 'Nick' });
-});
-
 module.exports = router;
